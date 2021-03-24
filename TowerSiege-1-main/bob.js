0f1b6e24b1c8fb726ae932884cef96e2282bbc14
Dominicarmijo <76975333+Dominicarmijo@users.noreply.github.com>
2021-03-24 15:48:26 -0600

    Add files via upload

---
+++ TowerSiege-1-main/bob.js
@@ -0,0 +1,26 @@
+class Bob {
+    constructor(x, y, r) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':0.04,
+      }
+      this.body = Bodies.circle(x, y, r, options);
+      this.r = r;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipseMode(CENTER);
+      strokeWeight(4);
+      stroke("green");
+      fill(255);
+      ellipse(0, 0, this.r, this.r);
+      pop();
+    }
+  }
